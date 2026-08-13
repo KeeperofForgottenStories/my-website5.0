105aef1a83278c66941ba5568de09f75d20a69af
KeeperofForgottenStories <thaismedice07@gmail.com>
2026-08-13 20:23:06 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,9 @@
+const SUPABASE_URL = "sb_publishable_ut77tWwxny5LjRw4uVuWaw_ISl_wJIv";
 
+const SUPABASE_ANON_KEY = "sb_secret_IZbVruPx88L0QbYTAixIIA_CSeAf4BO";
+
+const supabaseClient =
+    window.supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_ANON_KEY
+    );
